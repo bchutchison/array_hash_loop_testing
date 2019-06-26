21c929aba4a848ee94304c3d52f239cfac14ad53
andyymckay <andymckay1992@hotmail.co.uk>
2019-06-26 14:37:29 +0100

    My_functions_complete

---
+++ my_functions.rb
@@ -0,0 +1,21 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+
+def sum_array(numbers)
+  counter = 0
+  for number in numbers
+    counter += number
+  end
+  return counter
+end
+
+def find_item(houses, house)
+  houses.include?(house)
+end
+
+
+def get_first_key(hash)
+ return hash.keys[0]
+end
